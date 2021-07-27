66e0dd8849f7f90151c172f12b7dff2ca217bda6
lunnlew <4288315+lunnlew@users.noreply.github.com>
2021-07-27 12:03:09 +0800

    add demo

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <three-demo></three-demo>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import threeDemo from './components/threeDemo.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    threeDemo
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
